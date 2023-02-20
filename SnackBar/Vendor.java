4ff1657139325c723f1cc9e0d1aa2d145a8764c7
gargs1 <gargs@wlhs.wlwv.k12.or.us>
2023-02-20 12:08:04 -0800

    This is my first draft of the programming assignment- will make edits so it meets criteria.

--- SnackBar/Vendor.java
+++ SnackBar/Vendor.java
@@ -1,7 +1,19 @@
 package SnackBar;
 
 import Number42.Coins;
-
+/* Complete the Vendor constructor- done
+Complete the setStock mutator- done
+Complete the getStock accessor- done
+Complete the addMoney mutator-done
+Complete the getDeposit accessor- done
+Complete the makeSale method- Done
+Complete the getChangeString method- Done
+Complete the getTotalSales static method- Done
+Change the password to the prefix of your email (the part before the @).- done
+Submit Snackbar.java - done
+Submit Vendor.java - done
+Submit link to GitHub repository- done
+Follows Java coding convenstion https://google.github.io/styleguide/javaguide.html */
 /**
  * This class implements a vendor that sells one kind
  * of items. A vendor carries out sales transactions.
@@ -14,6 +26,8 @@ public class Vendor
     private int stock;
     private int deposit;
     private int change;
+
+    private static double totalSales = 0;
     //make a private static double variable called totalSales that has an initial value of 0
 
 
@@ -24,17 +38,20 @@ public class Vendor
      */
     public Vendor(int price, int stock)
     {
+        this.price = price;
+        this.stock = stock;
         //You need to complete this using this. notation
     }
 
     /**
      * Sets the quantity of items in stock.
-     * @param qty number of items to place in stock (int)
+     * @param stock number of items to place in stock (int)
      */
 
 
     public void setStock(int stock)
     {
+         this.stock = stock;
         //You need to complete this using this. notation
     }
 
@@ -45,15 +62,18 @@ public class Vendor
     public int getStock()
     {
         //complete this
+        return this.stock;
     }
 
     /**
      * Adds a specified amount (in cents) to the
      * deposited amount.
-     * @param number of cents to add to the deposit (int)
+     * @param d number of cents to add to the deposit (int)
      */
     public void addMoney(int d)
     {
+        deposit += d;
+        //Correctly adds money to the amount of money that will be given to the vending machine
         //You need to complete this using mutator
     }
 
@@ -63,6 +83,7 @@ public class Vendor
      */
     public int getDeposit()
     {
+
         return deposit;
     }
 
@@ -77,7 +98,17 @@ public class Vendor
      */
     public boolean makeSale()
     {
-        //create the makesale method
+        if(this.stock > 0 && this.deposit >= this.price)
+        {
+            this.stock -= 1;
+            this.change = this.deposit - this.price;
+            return true;
+        }
+        else {
+            change += this.deposit;
+            return false;
+        }
+
     }
 
     /**
@@ -98,10 +129,10 @@ public class Vendor
         /*
         note that the coin class has a .getQuarters(), a .getDimes() etc etc (use the coin class!)
         */
-        
-
-        String changeString="";
-
+        Coins coins = new Coins(change);
+        String changeString = "";
+        changeString =  "Your total change is " + coins.getPennies()+ " pennies, " + coins.getQuarters() + " quarters, " +
+        coins.getDimes() +" dimes, " + " and " + coins.getNickles() + " nickels.";
         return changeString;
     }
     
@@ -113,6 +144,16 @@ public class Vendor
     */
     public static double getTotalSales()
     {
+        double temp;
+        temp = totalSales;
+        totalSales = 0;
+        return temp;
+
+
+
+
+
+
         //complete this
     }
 }
